6a84af5936f91fe5e710f2a967ba40735ceef0c1
richpham <chillpham123@gmail.com>
2020-07-24 16:11:29 -0700

    Implement Google Maps

--- app/src/main/java/com/example/ecostyle/GoogleMapsActivity.java
+++ app/src/main/java/com/example/ecostyle/GoogleMapsActivity.java
@@ -11,21 +11,13 @@ import com.google.android.material.snackbar.Snackbar;
 
 public class GoogleMapsActivity extends AppCompatActivity {
 
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_maps);
-        Toolbar toolbar = findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
 
 }
